9264e6816bc75f410db747f35ada2214b4c3a6e4
Евгений Минин <evgeny.frontend@gmail.com>
2019-03-11 21:57:34 +0300

    add: get random planet

--- src/components/RandomPlanet/RandomPlanet.jsx
+++ src/components/RandomPlanet/RandomPlanet.jsx
@@ -1,26 +1,64 @@
 import React, { Component } from 'react';
+import SwapiService from './../../services/SwapiService';
 import './RandomPlanet.scss';
 
 class RandomPlanet extends Component {
-    state = {  }
-    render() { 
+
+    constructor() {
+        super();
+        this.updatePlanet();
+    };
+
+    swapiService = new SwapiService();
+
+    state = {
+        id: null,
+        planetName: null,
+        population: null,
+        climate: null,
+        rotationPeriod: null,
+        diameter: null
+    };
+
+    updatePlanet() {
+        const id = Math.floor(Math.random()*20) + 2;
+        this.swapiService
+            .getPlanet(id)
+            .then((planet) => {
+                this.setState({
+                    id,
+                    planetName: planet.name,
+                    population: planet.population,
+                    climate: planet.climate,
+                    rotationPeriod: planet.rotation_period,
+                    diameter: planet.diameter
+                });
+            });
+    }
+
+    render() {
+        const { id, planetName, population, climate, rotationPeriod, diameter } = this.state;
         return (
             <div className="random-planet jumbotron rounded">
-                <img src="https://starwars-visualguide.com/assets/img/planets/5.jpg" alt="" className="random-planet__image"/>
+                <img src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`} alt="" className="random-planet__image"/>
                 <div className="random-planet__info">
-                    <h4>Planet Name</h4>
+                    <h4>{planetName}</h4>
                     <ul className="list-group list-group-flush">
                         <li className="list-group-item custom-item">
                             <span className="list-group-item__title">Population:</span>
-                            <span>1234123</span>
+                            <span>{population}</span>
+                        </li>
+                        <li className="list-group-item custom-item">
+                            <span className="list-group-item__title">Climate:</span>
+                            <span>{climate}</span>
                         </li>
                         <li className="list-group-item custom-item">
                             <span className="list-group-item__title">Rotation Period:</span>
-                            <span>43</span>
+                            <span>{rotationPeriod}</span>
                         </li>
                         <li className="list-group-item custom-item">
                             <span className="list-group-item__title">Diametr:</span>
-                            <span>100</span>
+                            <span>{diameter}</span>
                         </li>
                     </ul>
                 </div>
